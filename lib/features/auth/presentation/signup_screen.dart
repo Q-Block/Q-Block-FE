61912fdc606359424f6034aa1eb048b920f664a4
이민경(Minkyung Lee) <87940552+Minlee01@users.noreply.github.com>
2024-10-05 06:01:44 +0900

    로그인_연결

--- lib/features/auth/presentation/signup_screen.dart
+++ lib/features/auth/presentation/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qblock_fe/features/auth/domain/auth_service.dart';
 import '../../../widgets/auth_dialog.dart';
 import '../../../widgets/navigationbar.dart';
 import '../../home/presentation/home_screen.dart';
@@ -15,12 +16,12 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _nicknameController = TextEditingController();
+  final AuthService _authService = AuthService();
 
-  void _onSignupPressed() {
+  Future<void> _onSignupPressed() async {
     if (_emailController.text.isEmpty ||
         _passwordController.text.isEmpty ||
         _nicknameController.text.isEmpty) {
-      // 입력값이 비어있을 경우 유효성 검사 다이얼로그 표시
       showDialog(
         context: context,
         builder: (context) {
@@ -32,21 +33,43 @@ class _SignupScreenState extends State<SignupScreen> {
         },
       );
     } else {
-      // 회원가입 성공 다이얼로그 표시
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AuthDialog(
-            title: '회원가입 성공',
-            content: '회원가입이 완료되었습니다.',
-            onConfirm: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => const LogInScreen()),
-              );
-            },
-          );
-        },
-      );
+      try {
+        print(
+            'Signup details: ${_emailController.text}, ${_passwordController.text}, ${_nicknameController.text}');
+
+        await _authService.signUp(
+          _emailController.text,
+          _passwordController.text,
+          _nicknameController.text,
+        );
+
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AuthDialog(
+              title: '회원가입 성공',
+              content: '회원가입이 완료되었습니다.',
+              onConfirm: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const LogInScreen()),
+                );
+              },
+            );
+          },
+        );
+      } catch (error) {
+        print('Signup failed with error: $error');
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AuthDialog(
+              title: '회원가입 실패',
+              content: '회원가입에 실패했습니다.',
+              onConfirm: () {},
+            );
+          },
+        );
+      }
     }
   }
 
@@ -73,12 +96,14 @@ class _SignupScreenState extends State<SignupScreen> {
                       vertical: 10.0, horizontal: 12.0),
                   labelText: "예: qblock@example.com",
                   filled: true,
-                  fillColor: const Color.fromRGBO(250, 250, 250, 0.7).withOpacity(0.2),
+                  fillColor:
+                      const Color.fromRGBO(250, 250, 250, 0.7).withOpacity(0.2),
                   labelStyle: TextStyle(color: Colors.black.withOpacity(0.6)),
                   floatingLabelBehavior: FloatingLabelBehavior.never,
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(4.0),
-                    borderSide: const BorderSide(width: 1, color: Colors.redAccent),
+                    borderSide:
+                        const BorderSide(width: 1, color: Colors.redAccent),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(4.0),
@@ -97,7 +122,8 @@ class _SignupScreenState extends State<SignupScreen> {
                 ),
                 style: TextStyle(color: Colors.black.withOpacity(0.6)),
               ),
-              const SizedBox(height: 20), // Space between TextField and next text
+              const SizedBox(
+                  height: 20), // Space between TextField and next text
               const Text("비밀번호*"),
               const Padding(padding: EdgeInsets.only(bottom: 5)),
               TextField(
@@ -107,12 +133,14 @@ class _SignupScreenState extends State<SignupScreen> {
                       vertical: 10.0, horizontal: 12.0),
                   labelText: "비밀번호를 입력해주세요",
                   filled: true,
-                  fillColor: const Color.fromRGBO(250, 250, 250, 0.7).withOpacity(0.2),
+                  fillColor:
+                      const Color.fromRGBO(250, 250, 250, 0.7).withOpacity(0.2),
                   labelStyle: TextStyle(color: Colors.black.withOpacity(0.6)),
                   floatingLabelBehavior: FloatingLabelBehavior.never,
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(4.0),
-                    borderSide: const BorderSide(width: 1, color: Colors.redAccent),
+                    borderSide:
+                        const BorderSide(width: 1, color: Colors.redAccent),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(4.0),
@@ -131,7 +159,8 @@ class _SignupScreenState extends State<SignupScreen> {
                 ),
                 style: TextStyle(color: Colors.black.withOpacity(0.6)),
               ),
-              const SizedBox(height: 20), // Space between TextField and next text
+              const SizedBox(
+                  height: 20), // Space between TextField and next text
               const Text("닉네임*"),
               const Padding(padding: EdgeInsets.only(bottom: 5)),
               TextField(
@@ -141,12 +170,14 @@ class _SignupScreenState extends State<SignupScreen> {
                       vertical: 10.0, horizontal: 12.0),
                   labelText: "닉네임을 입력해주세요",
                   filled: true,
-                  fillColor: const Color.fromRGBO(250, 250, 250, 0.7).withOpacity(0.2),
+                  fillColor:
+                      const Color.fromRGBO(250, 250, 250, 0.7).withOpacity(0.2),
                   labelStyle: TextStyle(color: Colors.black.withOpacity(0.6)),
                   floatingLabelBehavior: FloatingLabelBehavior.never,
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(4.0),
-                    borderSide: const BorderSide(width: 1, color: Colors.redAccent),
+                    borderSide:
+                        const BorderSide(width: 1, color: Colors.redAccent),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(4.0),
@@ -165,7 +196,9 @@ class _SignupScreenState extends State<SignupScreen> {
                 ),
                 style: TextStyle(color: Colors.black.withOpacity(0.6)),
               ),
-              const SizedBox(height: 100), // Space between TextField and button (버튼을 더 아래로 이동시킴)
+              const SizedBox(
+                  height:
+                      100), // Space between TextField and button (버튼을 더 아래로 이동시킴)
               Center(
                 child: SizedBox(
                   width: double.infinity,
@@ -180,7 +213,8 @@ class _SignupScreenState extends State<SignupScreen> {
                     onPressed: _onSignupPressed,
                     child: const Text(
                       "가입하기",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.normal),
                       textAlign: TextAlign.center,
                     ),
                   ),
